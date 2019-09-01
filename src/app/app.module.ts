984d5b0ee5abb5e0d2265e2e64b6b2c707a1442a
ADaanCruz <djdaan86@gmail.com>
2019-09-01 16:32:45 -0500

    Connection with Firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Firebase
+// import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+// import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+// import { AngularFireModule } from 'angularfire2';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireDatabaseModule,
+    // AngularFireAuth,
     AppRoutingModule
   ],
   providers: [],
